a4563dd01e3edaf6de598b683dfd67d91f4b3ece
Brennan Doehring <brennandoehringd@outlook.com>
2020-09-08 19:54:38 -0400

    Linked .js to .html file, current date display functioning properly.

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 // Initializes upon page opening
 $(document).ready(function() {
     console.log("Let's begin!");
-})
+
+    // Displays the current day at the bottom of the header
+    $('#currentDay').text(moment().format('LL'));
+    console.log("Date has been pushed.");
+});
