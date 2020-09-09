e1992eb5b6792dd3cdfa28f757b903f3ae8d3c36
Brennan Doehring <brennandoehringd@outlook.com>
2020-09-08 20:45:41 -0400

    Created the table. Styling not working properly.

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// Time array
+var timeArray = {
+    "9AM": "", 
+    "10AM": "",
+    "11AM": "",
+    "12PM": "",
+    "1PM": "",
+    "2PM": "",
+    "3PM": "",
+    "4PM": "",
+    "5PM": "",
+};
+
 // Initializes upon page opening
 $(document).ready(function() {
     console.log("Let's begin!");
@@ -5,4 +18,11 @@ $(document).ready(function() {
     // Displays the current day at the bottom of the header
     $('#currentDay').text(moment().format('LL'));
     console.log("Date has been pushed.");
+
+    // Changes timeblock colors
+    function timeblockColorSet() {
+        console.log("Initializing timeblockColorSet function.");
+        
+
+    }
 });
